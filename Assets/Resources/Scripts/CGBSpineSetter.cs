f13782d38c89f23312ea446a9eb6e73050c08761
sujep <sujep4a@naver.com>
2022-03-18 12:58:23 +0900

    차깨비스파인 마스크 적용
    차깨비인벤토리에서 스크롤 마스크에 스파인 적용 됨

--- Assets/Resources/Scripts/CGBSpineSetter.cs
+++ Assets/Resources/Scripts/CGBSpineSetter.cs
@@ -6,6 +6,7 @@ using Spine.Unity;
 public class CGBSpineSetter : MonoBehaviour
 {
     public SkeletonAnimation skeletonAnimation;
+    public SkeletonGraphic skeletonGraphic;
 
     #region »ö,½½·ÔÀÌ¸§ ¼³Á¤
     //Â÷±úºñ ¸ö»ö º° ´«½ç,º¼ÅÍÄ¡»ö
@@ -28,38 +29,77 @@ public class CGBSpineSetter : MonoBehaviour
 
     public void SetAppearance(CGBData cgb) //Â÷±úºñ½ºÆÄÀÎ¿¡ ¿ÜÇü ¼³Á¤ÇÏ±â
     {
-        skeletonAnimation.Skeleton.SetSkin(cgb.age + "_" + cgb.skin.ToString());
-        skeletonAnimation.Skeleton.SetAttachment(bodySlot, "1/body_" + cgb.bodyColor.ToString());
-        skeletonAnimation.Skeleton.SetAttachment(leg1Slot, "1/leg_" + cgb.bodyColor.ToString());
-        skeletonAnimation.Skeleton.SetAttachment(leg2Slot, "1/leg_" + cgb.bodyColor.ToString());
-        skeletonAnimation.Skeleton.SetAttachment(leg3Slot, "1/leg_" + cgb.bodyColor.ToString());
-        skeletonAnimation.Skeleton.SetAttachment(leg4Slot, "1/leg_" + cgb.bodyColor.ToString());
-        skeletonAnimation.Skeleton.SetAttachment(mouthSlot, "face/mouth_" + cgb.mouth);
-        skeletonAnimation.Skeleton.SetAttachment(browRSlot, "face/brow_" + cgb.brow);
-        skeletonAnimation.Skeleton.SetAttachment(browLSlot, "face/brow_" + cgb.brow);
-        //´«½ç, º¼ »ö º¯°æ
-        switch (cgb.bodyColor)
+        if (skeletonAnimation != null)
         {
-            case CGBData.colors.green:
-                skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(greenBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(greenBrow);
-                skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(greenCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(greenCheek);
-                break;
+            skeletonAnimation.Skeleton.SetSkin(cgb.age + "_" + cgb.skin.ToString());
+            skeletonAnimation.Skeleton.SetAttachment(bodySlot, "1/body_" + cgb.bodyColor.ToString());
+            skeletonAnimation.Skeleton.SetAttachment(leg1Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonAnimation.Skeleton.SetAttachment(leg2Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonAnimation.Skeleton.SetAttachment(leg3Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonAnimation.Skeleton.SetAttachment(leg4Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonAnimation.Skeleton.SetAttachment(mouthSlot, "face/mouth_" + cgb.mouth);
+            skeletonAnimation.Skeleton.SetAttachment(browRSlot, "face/brow_" + cgb.brow);
+            skeletonAnimation.Skeleton.SetAttachment(browLSlot, "face/brow_" + cgb.brow);
+            //´«½ç, º¼ »ö º¯°æ
+            switch (cgb.bodyColor)
+            {
+                case CGBData.colors.green:
+                    skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(greenBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(greenBrow);
+                    skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(greenCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(greenCheek);
+                    break;
 
-            case CGBData.colors.yellow:
-                skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(yellowBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(yellowBrow);
-                skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(yellowCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(yellowCheek);
-                break;
+                case CGBData.colors.yellow:
+                    skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(yellowBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(yellowBrow);
+                    skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(yellowCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(yellowCheek);
+                    break;
 
-            case CGBData.colors.red:
-                skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(redBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(redBrow);
-                skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(redCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(redCheek);
-                break;
+                case CGBData.colors.red:
+                    skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(redBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(redBrow);
+                    skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(redCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(redCheek);
+                    break;
 
-            case CGBData.colors.brown:
-                skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(brownBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(brownBrow);
-                skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(brownCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(brownCheek);
-                break;
+                case CGBData.colors.brown:
+                    skeletonAnimation.Skeleton.FindSlot(browRSlot).SetColor(brownBrow); skeletonAnimation.Skeleton.FindSlot(browLSlot).SetColor(brownBrow);
+                    skeletonAnimation.Skeleton.FindSlot(cheekRSlot).SetColor(brownCheek); skeletonAnimation.Skeleton.FindSlot(cheekLSlot).SetColor(brownCheek);
+                    break;
+            }
         }
+        else if (skeletonGraphic != null)
+        {
+            skeletonGraphic.Skeleton.SetSkin(cgb.age + "_" + cgb.skin.ToString());
+            skeletonGraphic.Skeleton.SetAttachment(bodySlot, "1/body_" + cgb.bodyColor.ToString());
+            skeletonGraphic.Skeleton.SetAttachment(leg1Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonGraphic.Skeleton.SetAttachment(leg2Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonGraphic.Skeleton.SetAttachment(leg3Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonGraphic.Skeleton.SetAttachment(leg4Slot, "1/leg_" + cgb.bodyColor.ToString());
+            skeletonGraphic.Skeleton.SetAttachment(mouthSlot, "face/mouth_" + cgb.mouth);
+            skeletonGraphic.Skeleton.SetAttachment(browRSlot, "face/brow_" + cgb.brow);
+            skeletonGraphic.Skeleton.SetAttachment(browLSlot, "face/brow_" + cgb.brow);
+            //´«½ç, º¼ »ö º¯°æ
+            switch (cgb.bodyColor)
+            {
+                case CGBData.colors.green:
+                    skeletonGraphic.Skeleton.FindSlot(browRSlot).SetColor(greenBrow); skeletonGraphic.Skeleton.FindSlot(browLSlot).SetColor(greenBrow);
+                    skeletonGraphic.Skeleton.FindSlot(cheekRSlot).SetColor(greenCheek); skeletonGraphic.Skeleton.FindSlot(cheekLSlot).SetColor(greenCheek);
+                    break;
+
+                case CGBData.colors.yellow:
+                    skeletonGraphic.Skeleton.FindSlot(browRSlot).SetColor(yellowBrow); skeletonGraphic.Skeleton.FindSlot(browLSlot).SetColor(yellowBrow);
+                    skeletonGraphic.Skeleton.FindSlot(cheekRSlot).SetColor(yellowCheek); skeletonGraphic.Skeleton.FindSlot(cheekLSlot).SetColor(yellowCheek);
+                    break;
+
+                case CGBData.colors.red:
+                    skeletonGraphic.Skeleton.FindSlot(browRSlot).SetColor(redBrow); skeletonGraphic.Skeleton.FindSlot(browLSlot).SetColor(redBrow);
+                    skeletonGraphic.Skeleton.FindSlot(cheekRSlot).SetColor(redCheek); skeletonGraphic.Skeleton.FindSlot(cheekLSlot).SetColor(redCheek);
+                    break;
+
+                case CGBData.colors.brown:
+                    skeletonGraphic.Skeleton.FindSlot(browRSlot).SetColor(brownBrow); skeletonGraphic.Skeleton.FindSlot(browLSlot).SetColor(brownBrow);
+                    skeletonGraphic.Skeleton.FindSlot(cheekRSlot).SetColor(brownCheek); skeletonGraphic.Skeleton.FindSlot(cheekLSlot).SetColor(brownCheek);
+                    break;
+            }
+        }       
+        
     }
 
 }
