958287d759ab97081d6896f425ed5afbdd39cc85
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2022-05-19 23:09:05 +0900

    1

---
+++ Assets/Resources/Scripts/MiniGame.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGame : MonoBehaviour
+{
+
+    public Button Cup1;
+    public Button Cup2;
+    public Button Cup3;
+    public Button Next;
+    public Text Selected;
+    public GameObject CupPanel;
+    public GameObject TeabagPanel;
+    public GameObject CupinTeabag;
+    public Image Cupimage;
+
+    public int CupSel = 0;
+    public int CupResult = 0;
+
+    public void SelectCup1()
+    {
+        Selected.text = "1¹øÄÅ ¼±ÅÃµÊ";
+        CupSel = 1;
+
+    }
+    public void SelectCup2()
+    {
+        Selected.text = "2¹øÄÅ ¼±ÅÃµÊ";
+        CupSel = 2;
+
+    }
+    public void SelectCup3()
+    {
+        Selected.text = "3¹øÄÅ ¼±ÅÃµÊ";
+        CupSel = 3;
+
+    }
+
+    public void ResultCup()
+    {
+        CupResult = CupSel;
+        if (CupResult != 0)
+        {
+            CupPanel.SetActive(false);
+            TeabagPanel.SetActive(true);
+        }
+        else
+            Selected.text = "ÄÅÀ» °í¸£½Ã¿À";
+
+    }
+
+    public void TeabagCupSel()
+    {
+        if(CupResult == 0)
+            Cupimage.sprite = Resources.Load<Sprite>("Images/cup1") as Sprite;
+
+        else if (CupResult == 1)
+            Cupimage.sprite = Resources.Load<Sprite>("Images/cup1") as Sprite;
+        
+        else if (CupResult == 2)
+            Cupimage.sprite = Resources.Load<Sprite>("Images/cup2") as Sprite;
+        
+        else if (CupResult == 3)
+            Cupimage.sprite = Resources.Load<Sprite>("Images/cup3") as Sprite;
+
+
+    }
+
+
+
+    void Start()
+    {
+        Cup1.onClick.AddListener(SelectCup1);
+        Cup2.onClick.AddListener(SelectCup2);
+        Cup3.onClick.AddListener(SelectCup3);
+        Next.onClick.AddListener(ResultCup);
+    }
+
+
+    void Update()
+    {
+        TeabagCupSel();
+    }
+}
