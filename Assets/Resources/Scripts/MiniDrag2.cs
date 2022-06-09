4ff98cab2481ee6383866a5714c4f473c477058b
jihwanjeong <53459838+jihwanjeong@users.noreply.github.com>
2022-06-09 17:28:52 +0900

    1

--- Assets/Resources/Scripts/MiniDrag2.cs
+++ Assets/Resources/Scripts/MiniDrag2.cs
@@ -13,8 +13,14 @@ public class MiniDrag2 : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     [SerializeField] Canvas canvas;
     public GameObject TC1;
     public GameObject TC2;
+    public GameObject TC3;
+    public GameObject TC4;
+    public GameObject TC5;
+    public GameObject TC6;
     public GameObject T1;
     public GameObject T2;
+    public GameObject T1pos;
+    public GameObject T2pos;
     public Text Ttext;
     
 
@@ -51,18 +57,35 @@ public class MiniDrag2 : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
 
     public void OnDrop(PointerEventData eventData)
     {
-        if (this.transform.localPosition.x > -126 && this.transform.localPosition.x < 126)
+        if ((this.transform.localPosition.x > -187 && this.transform.localPosition.x < -87)&& (this.transform.localPosition.y > -50 && this.transform.localPosition.y < 50))
         {
             TC1.SetActive(true);
-            T1.SetActive(false);
+            //T1.SetActive(false);
             
+
+
+        }
+       
+        if ((this.transform.localPosition.x > -50 && this.transform.localPosition.x < 50) && (this.transform.localPosition.y > -50 && this.transform.localPosition.y < 50))
+        {
+            TC3.SetActive(true);
+            //T1.SetActive(false);
+
+
+
+        }
+        if ((this.transform.localPosition.x > 86 && this.transform.localPosition.x < 186) && (this.transform.localPosition.y > -50 && this.transform.localPosition.y < 50))
+        {
+            TC5.SetActive(true);
+            //T1.SetActive(false);
+
 
 
         }
     }
     void Update()
     {
-        if (TC1.activeSelf == true && TC2.activeSelf == false)
+       /* if (TC1.activeSelf == true && TC2.activeSelf == false)
             Ttext.text = "³ì»öÂ÷";
          else if (TC1.activeSelf == false && TC2.activeSelf == true)
             Ttext.text = "»¡°­Â÷";
@@ -70,7 +93,7 @@ public class MiniDrag2 : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
             Ttext.text = "»¡°­³ì»öÂ÷";
          else if (TC1.activeSelf == false && TC2.activeSelf == false)
             Ttext.text = "ºóÄÅ";
-
+       */
 
     }
     
